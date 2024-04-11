920e4cd54810b68b429090c59117402dfdd8fbf6
Cameron Angelilli <115901280+Camerxns@users.noreply.github.com>
2024-04-11 12:10:40 -0400

    Updated

---
+++ Wedding-Planner/app/Models/answers.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class answers extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'answer1',
+        'fullName',
+        'answer2',
+        'potluck'
+    ];
+}
